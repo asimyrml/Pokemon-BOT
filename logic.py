c5eecf0d772d27972998488fde92a62a3b02852d
asimyrml <asm14yrml@gmail.com>
2025-07-15 14:54:48 +0300

    first commit

---
+++ logic.py
@@ -0,0 +1,74 @@
+import aiohttp  # Eşzamansız HTTP istekleri için bir kütüphane
+import random
+
+class Pokemon:
+    pokemons = {}
+    # Nesne başlatma (kurucu)
+    def __init__(self, pokemon_trainer):
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        self.hp = random.randint(1, 100)
+        self.power = random.randint(1, 10)
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
+        else:
+            self = Pokemon.pokemons[pokemon_trainer]
+
+    async def get_name(self):
+        # PokeAPI aracılığıyla bir pokémonun adını almak için asenktron metot
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # İstek için URL API
+        async with aiohttp.ClientSession() as session:  #  HTTP oturumu açma
+            async with session.get(url) as response:  # GET isteği gönderme
+                if response.status == 200:
+                    data = await response.json()  # JSON yanıtının alınması ve çözümlenmesi
+                    return data['forms'][0]['name']  #  Pokémon adını döndürme
+                else:
+                    return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
+                
+    async def attack(self, enemy): 
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu şimdi {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+
+
+    async def info(self):
+        # Pokémon hakkında bilgi döndüren bir metot
+        if not self.name:
+            self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
+        return f"Pokémonunuzun ismi: {self.name}\n Pokemon HP: {self.hp}\n Pokemon Power {self.power}"  # Pokémon adını içeren dizeyi döndürür
+
+    async def show_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    img_url = data['sprites']['front_default']
+                    return img_url
+                else:
+                    return None
+                
+class Wizard(Pokemon):
+    async def attack(self, enemy):
+        if isinstance(enemy, Wizard):
+            chance = random.randint(1, 5)
+            if chance == 1:
+                return "Sihirbaz Pokémon, savaşta bir kalkan kullandı!"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu şimdi {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_power = random.randint(5, 15)
+        self.power += super_power
+        result = await super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güc: {super_power}"
