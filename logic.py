7608a78807b388e8916dbf21b0b6514c59615c23
asimyrml <asm14yrml@gmail.com>
2025-07-15 21:09:33 +0300

    Pokemon Attack Functions added

--- logic.py
+++ logic.py
@@ -8,6 +8,8 @@ class Pokemon:
         self.pokemon_trainer = pokemon_trainer
         self.pokemon_number = random.randint(1, 1000)
         self.name = None
+        self.hp = random.randint(1, 100)  # Pokémon'un can puanı
+        self.power = random.randint(1, 10)  # Pokémon'un gücü
         if pokemon_trainer not in Pokemon.pokemons:
             Pokemon.pokemons[pokemon_trainer] = self
         else:
@@ -24,11 +26,19 @@ class Pokemon:
                 else:
                     return "Pikachu"  # İstek başarısız olursa varsayılan adı döndürür
 
+    async def attack(self, enemy):
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+
     async def info(self):
         # Pokémon hakkında bilgi döndüren bir metot
         if not self.name:
             self.name = await self.get_name()  # Henüz yüklenmemişse bir adın geri alınması
-        return f"Pokémonunuzun ismi: {self.name}"  # Pokémon adını içeren dizeyi döndürür
+        return f"Pokémonunuzun ismi: {self.name} \n Pokemon HP: {self.hp} \n Pokemon Power {self.power}"  # Pokémon adını içeren dizeyi döndürür
 
     async def show_img(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -40,3 +50,24 @@ class Pokemon:
                     return img_url
                 else:
                     return None
+    
+class Wizard(Pokemon):
+    async def attack(self, enemy):
+        if isinstance(enemy, Wizard):  # Düşmanın Wizard veri tipi olup olmadığının kontrol edilmesi (Sihirbaz sınıfının bir örneği midir?) 
+            sans = random.randint(1, 5) 
+            if sans == 1:
+                return "Sihirbaz Pokémon, savaşta bir kalkan kullanıldı!"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ne saldırdı\n@{enemy.pokemon_trainer}'nin sağlık durumu {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Pokémon eğitmeni @{self.pokemon_trainer} @{enemy.pokemon_trainer}'ni yendi!"
+
+class Fighter(Pokemon):
+    async def attack(self, enemy):
+        super_guc = random.randint(5, 15)  
+        self.guc += super_guc
+        sonuc = await super().attack(enemy)  
+        self.guc -= super_guc
+        return sonuc + f"\nDövüşçü Pokémon süper saldırı kullandı. Eklenen güç: {super_guc}"
